6471b7dde48cd0bec7447efd127904602299cfa9
Romain “Artefact2” Dal Maso <romain.dalmaso@artefact2.com>
2024-03-01 09:12:27 +0100

    backend: fix anti join in voting_pairs

--- schema.sql
+++ schema.sql
@@ -51,10 +51,11 @@ CREATE VIEW voting_pairs AS
 SELECT sessions.session_id, prompts.prompt_id, prompt, a.model_name AS model_name_a, b.model_name AS model_name_b, a.answer AS answer_a, b.answer AS answer_b
 FROM prompts -- for all prompts...
 JOIN sessions -- and all sessions...
-JOIN answers AS a ON a.prompt_id = prompts.prompt_id -- grab all the answers
-LEFT JOIN votes AS va ON va.session_id = sessions.session_id AND va.prompt_id = prompts.prompt_id AND (va.model_name_a = a.model_name OR va.model_name_b = a.model_name) AND va.session_id IS NULL -- anti join already voted-on answers
-JOIN answers AS b ON b.prompt_id = prompts.prompt_id AND a.model_name < b.model_name
-LEFT JOIN votes AS vb ON vb.session_id = sessions.session_id AND vb.prompt_id = prompts.prompt_id AND (vb.model_name_a = b.model_name OR vb.model_name_b = b.model_name) AND vb.session_id IS NULL;
+JOIN answers AS a ON a.prompt_id = prompts.prompt_id -- join first answer
+LEFT JOIN votes AS va ON va.session_id = sessions.session_id AND va.prompt_id = prompts.prompt_id AND a.model_name IN(va.model_name_a, va.model_name_b)
+JOIN answers AS b ON b.prompt_id = prompts.prompt_id AND a.model_name < b.model_name -- join second answer
+LEFT JOIN votes AS vb ON vb.session_id = sessions.session_id AND vb.prompt_id = prompts.prompt_id AND b.model_name IN(vb.model_name_a, vb.model_name_b)
+WHERE va.session_id IS NULL AND vb.session_id IS NULL; -- anti join already answered models
 
 CREATE VIEW results AS
 SELECT COUNT(session_id)
