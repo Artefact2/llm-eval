fc9d4ee44a7debadc2f01646ad17782a4bcc059d
Romain “Artefact2” Dal Maso <romain.dalmaso@artefact2.com>
2024-03-01 01:23:13 +0100

    backend: fix generate-answers server args

--- config-example.php
+++ config-example.php
@@ -4,7 +4,7 @@ return [
 	/* generate one yourself, with eg $(xxd -c 32 -p -l 32 /dev/urandom) */
 	'hmac_secret' => '',
 
-	'server_args' => function(string $server_path): string {
+	'server_args' => function(string $model_path): string {
 		return '-ngl 999';
 	},
 ];
