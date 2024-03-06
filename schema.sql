a1b2e8bd9f9b68e259ae52b19809481ecc480b9a
Romain “Artefact2” Dal Maso <romain.dalmaso@artefact2.com>
2024-03-06 18:23:12 +0100

    schema: materialize results_per_prompt, use median vote per prompt
    In place upgrade:
    
    BEGIN;
    
    DROP TRIGGER vote_insert;
    DROP TRIGGER vote_delete;
    DROP TRIGGER vote_update;
    DROP VIEW results_per_prompt;
    
    CREATE TABLE results_per_prompt (
    model_id_a INTEGER NOT NULL,
    model_id_b INTEGER NOT NULL,
    prompt_id INTEGER NOT NULL,
    median_vote INTEGER,
    n_votes INTEGER NOT NULL,
    PRIMARY KEY(model_id_a, model_id_b, prompt_id),
    FOREIGN KEY(model_id_a) REFERENCES models(model_id),
    FOREIGN KEY(model_id_b) REFERENCES models(model_id),
    FOREIGN KEY(prompt_id) REFERENCES prompts(prompt_id)
    ) WITHOUT ROWID;
    
    INSERT INTO results_per_prompt
    SELECT ma.model_id, mb.model_id, p.prompt_id, json_group_array(vote ORDER BY vote ASC) ->> CAST(0.5 * COUNT(vote) AS INTEGER), COUNT(vote)
    FROM models AS ma
    JOIN models AS mb ON ma.model_id < mb.model_id
    JOIN prompts AS p
    LEFT JOIN votes AS v ON v.model_id_a = ma.model_id AND v.model_id_b = mb.model_id AND v.prompt_id = p.prompt_id
    GROUP BY ma.model_id, mb.model_id, p.prompt_id;
    
    CREATE TRIGGER vote_insert AFTER INSERT ON votes BEGIN
    INSERT OR REPLACE INTO results_per_prompt(model_id_a, model_id_b, prompt_id, median_vote, n_votes)
    SELECT new.model_id_a, new.model_id_b, new.prompt_id, vote,
    (SELECT COUNT(*) FROM votes WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id)
    FROM votes
    WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id
    ORDER BY vote ASC
    LIMIT 1 OFFSET CAST(0.5 * (SELECT COUNT(*) FROM votes WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id) AS INTEGER);
    INSERT OR REPLACE INTO results_agg(model_id_a, model_id_b, s_votes, n_votes)
    SELECT model_id_a, model_id_b, SUM(median_vote), COUNT(median_vote)
    FROM results_per_prompt
    WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b
    GROUP BY model_id_a, model_id_b;
    END;
    
    CREATE TRIGGER vote_delete AFTER DELETE ON votes BEGIN
    DELETE FROM results_per_prompt WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id;
    INSERT OR REPLACE INTO results_per_prompt(model_id_a, model_id_b, prompt_id, median_vote, n_votes)
    SELECT new.model_id_a, new.model_id_b, new.prompt_id, vote,
    (SELECT COUNT(*) FROM votes WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id)
    FROM votes
    WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id
    ORDER BY vote ASC
    LIMIT 1 OFFSET CAST(0.5 * (SELECT COUNT(*) FROM votes WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id) AS INTEGER);
    INSERT OR REPLACE INTO results_agg(model_id_a, model_id_b, s_votes, n_votes)
    SELECT model_id_a, model_id_b, SUM(median_vote), COUNT(median_vote)
    FROM results_per_prompt
    WHERE model_id_a = old.model_id_a AND model_id_b = old.model_id_b
    GROUP BY model_id_a, model_id_b;
    END;
    
    CREATE TRIGGER vote_update AFTER UPDATE ON votes BEGIN
    INSERT OR REPLACE INTO results_per_prompt(model_id_a, model_id_b, prompt_id, median_vote, n_votes)
    SELECT new.model_id_a, new.model_id_b, new.prompt_id, vote,
    (SELECT COUNT(*) FROM votes WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id)
    FROM votes
    WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id
    ORDER BY vote ASC
    LIMIT 1 OFFSET CAST(0.5 * (SELECT COUNT(*) FROM votes WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id) AS INTEGER);
    INSERT OR REPLACE INTO results_agg(model_id_a, model_id_b, s_votes, n_votes)
    SELECT model_id_a, model_id_b, SUM(median_vote), COUNT(median_vote)
    FROM results_per_prompt
    WHERE (model_id_a = old.model_id_a AND model_id_b = old.model_id_b) OR (model_id_a = new.model_id_a AND model_id_b = new.model_id_b)
    GROUP BY model_id_a, model_id_b;
    END;
    
    COMMIT;

--- schema.sql
+++ schema.sql
@@ -57,6 +57,9 @@ CHECK(vote IN(-1, 0, 1)),
 CHECK(model_id_a < model_id_b)
 ) WITHOUT ROWID;
 
+CREATE INDEX votes_models_ab_session_id ON votes(session_id, model_id_a, model_id_b, vote);
+CREATE INDEX votes_models_ab_prompt_id ON votes(model_id_a, model_id_b, prompt_id, vote);
+
 CREATE VIEW voting_pairs AS
 SELECT sessions.session_id, prompts.prompt_id, prompt, a.model_id AS model_id_a, b.model_id AS model_id_b, a.answer AS answer_a, b.answer AS answer_b
 FROM prompts -- for all prompts...
@@ -67,23 +70,28 @@ JOIN answers AS b ON b.prompt_id = prompts.prompt_id AND a.model_id < b.model_id
 LEFT JOIN voted_answers AS vb ON vb.session_id = sessions.session_id AND vb.prompt_id = prompts.prompt_id AND vb.model_id = b.model_id
 WHERE va.session_id IS NULL AND vb.session_id IS NULL; -- anti join already voted on answers
 
-CREATE INDEX votes_models_ab_session_id ON votes(session_id, model_id_a, model_id_b, vote);
 CREATE VIEW results_per_session AS
 SELECT session_id, model_id_a, model_id_b, SUM(vote) AS s_votes, COUNT(vote) AS n_votes
 FROM votes
 GROUP BY session_id, model_id_a, model_id_b;
 
-CREATE INDEX votes_models_ab_prompt_id ON votes(model_id_a, model_id_b, prompt_id, vote);
-CREATE VIEW results_per_prompt AS
-SELECT model_id_a, model_id_b, prompt_id, AVG(vote) AS avg_vote
-FROM votes
-GROUP BY model_id_a, model_id_b, prompt_id;
+CREATE TABLE results_per_prompt (
+model_id_a INTEGER NOT NULL,
+model_id_b INTEGER NOT NULL,
+prompt_id INTEGER NOT NULL,
+median_vote INTEGER,
+n_votes INTEGER NOT NULL,
+PRIMARY KEY(model_id_a, model_id_b, prompt_id),
+FOREIGN KEY(model_id_a) REFERENCES models(model_id),
+FOREIGN KEY(model_id_b) REFERENCES models(model_id),
+FOREIGN KEY(prompt_id) REFERENCES prompts(prompt_id)
+) WITHOUT ROWID;
 
 CREATE TABLE results_agg (
-model_id_a INTEGER,
-model_id_b INTEGER,
-s_votes REAL,
-n_votes INTEGER,
+model_id_a INTEGER NOT NULL,
+model_id_b INTEGER NOT NULL,
+s_votes INTEGER NOT NULL,
+n_votes INTEGER NOT NULL,
 PRIMARY KEY(model_id_a, model_id_b),
 FOREIGN KEY(model_id_a) REFERENCES models(model_id),
 FOREIGN KEY(model_id_b) REFERENCES models(model_id),
@@ -93,24 +101,46 @@ CHECK(model_id_a < model_id_b)
 CREATE INDEX results_agg_n_votes_idx ON results_agg(n_votes);
 
 CREATE TRIGGER vote_insert AFTER INSERT ON votes BEGIN
+INSERT OR REPLACE INTO results_per_prompt(model_id_a, model_id_b, prompt_id, median_vote, n_votes)
+SELECT new.model_id_a, new.model_id_b, new.prompt_id, vote,
+(SELECT COUNT(*) FROM votes WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id)
+FROM votes
+WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id
+ORDER BY vote ASC
+LIMIT 1 OFFSET CAST(0.5 * (SELECT COUNT(*) FROM votes WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id) AS INTEGER);
 INSERT OR REPLACE INTO results_agg(model_id_a, model_id_b, s_votes, n_votes)
-SELECT model_id_a, model_id_b, SUM(avg_vote), COUNT(avg_vote)
+SELECT model_id_a, model_id_b, SUM(median_vote), COUNT(median_vote)
 FROM results_per_prompt
 WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b
 GROUP BY model_id_a, model_id_b;
 END;
 
 CREATE TRIGGER vote_delete AFTER DELETE ON votes BEGIN
+DELETE FROM results_per_prompt WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id;
+INSERT OR REPLACE INTO results_per_prompt(model_id_a, model_id_b, prompt_id, median_vote, n_votes)
+SELECT new.model_id_a, new.model_id_b, new.prompt_id, vote,
+(SELECT COUNT(*) FROM votes WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id)
+FROM votes
+WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id
+ORDER BY vote ASC
+LIMIT 1 OFFSET CAST(0.5 * (SELECT COUNT(*) FROM votes WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id) AS INTEGER);
 INSERT OR REPLACE INTO results_agg(model_id_a, model_id_b, s_votes, n_votes)
-SELECT model_id_a, model_id_b, SUM(avg_vote), COUNT(avg_vote)
+SELECT model_id_a, model_id_b, SUM(median_vote), COUNT(median_vote)
 FROM results_per_prompt
 WHERE model_id_a = old.model_id_a AND model_id_b = old.model_id_b
 GROUP BY model_id_a, model_id_b;
 END;
 
 CREATE TRIGGER vote_update AFTER UPDATE ON votes BEGIN
+INSERT OR REPLACE INTO results_per_prompt(model_id_a, model_id_b, prompt_id, median_vote, n_votes)
+SELECT new.model_id_a, new.model_id_b, new.prompt_id, vote,
+(SELECT COUNT(*) FROM votes WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id)
+FROM votes
+WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id
+ORDER BY vote ASC
+LIMIT 1 OFFSET CAST(0.5 * (SELECT COUNT(*) FROM votes WHERE model_id_a = new.model_id_a AND model_id_b = new.model_id_b AND prompt_id = new.prompt_id) AS INTEGER);
 INSERT OR REPLACE INTO results_agg(model_id_a, model_id_b, s_votes, n_votes)
-SELECT model_id_a, model_id_b, SUM(avg_vote), COUNT(avg_vote)
+SELECT model_id_a, model_id_b, SUM(median_vote), COUNT(median_vote)
 FROM results_per_prompt
 WHERE (model_id_a = old.model_id_a AND model_id_b = old.model_id_b) OR (model_id_a = new.model_id_a AND model_id_b = new.model_id_b)
 GROUP BY model_id_a, model_id_b;
